f7e9d97e71ab57a37f0bdb56bf7b2985f648da08
louisonbrault <braultlouison@gmail.com>
2023-07-01 16:18:32 -1000

    clean rules

--- front/src/app/rules/rules.component.ts
+++ front/src/app/rules/rules.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rules',
   templateUrl: './rules.component.html',
-  styleUrls: ['./rules.component.scss']
+  styleUrls: ['./rules.component.scss', '../app.component.scss']
 })
 export class RulesComponent {
 
